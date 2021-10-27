c7378468f344f0580246db2a2a4b4f7747295398
Gaya Kessler <gaya@theclevernode.com>
2021-10-27 08:37:07 +0200

    feat: add preact frontend

---
+++ src/client/App.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from 'preact';
+
+import './style.css';
+
+const App: FunctionComponent = () => {
+  return (
+    <div>Hello World</div>
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from 'preact';
+
+import App from './client/App';
+
+const Root: FunctionComponent = () => <App />;
+
+export default Root;
